6241fd6e14971a25bc96f3480983727649148c2a
Jimmy Jouanne <jimmy@liberkeys.com>
2022-01-06 09:56:06 +0100

    feat: add message if no country found for a specify region

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -40,6 +40,13 @@ export default function Home() {
 
       { !isLoading && error && <p className='text-dark-blue-text dark:text-neutrals-white text-center mt-6'>{ error }</p> }
       { isLoading && <p className='text-dark-blue-text dark:text-neutrals-white text-center mt-6'>Loading ...</p> }
+      { countries && 
+        countries
+          .filter(country => country.population !== 0)
+          .filter(country => regionFilter ? country.region === regionFilter.value : true)
+          .length === 0 && 
+        <p className='text-dark-blue-text dark:text-neutrals-white text-center mt-6'>No country for this region ...</p> 
+      }
 
       { countries && 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-9 my-9 mx-6 md:my-12 md:mx-0">
